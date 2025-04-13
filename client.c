b7d7f3ca0251a4d86fa5026dbc6403199b615221
niclee <nico@Zenaliu>
2025-04-13 06:32:18 +0200

    commentaire

--- client.c
+++ client.c
@@ -14,23 +14,23 @@
 #include "libft/libft.h"
 #include "ft_printf/ft_printf.h"
 
-void	send_signal(int pid, char c)
+void	send_signal(int pid, char c) // fonction qui permet d'envoyer un signal (SIGUSR1 ou SIGUSR2)
 {
 	int	i;
 
 	i = 0;
-	while (i < 8)
+	while (i < 8) // parcourir les 8 bits du caractère 
 	{
-		if ((c >> (7 - i)) & 1)
-			kill(pid, SIGUSR1);
+		if ((c >> (7 - i)) & 1) // decaler le bit de 7-i vers la droite et verifie si c'est un 1 ou un 0
+			kill(pid, SIGUSR1); // si c'est un 1, on envoie SIGUSR1
 		else
-			kill(pid, SIGUSR2);
-		usleep(100);
+			kill(pid, SIGUSR2); // si c'est un 0, on envoie SIGUSR2
+		usleep(100); // attendre 100 microsecondes entre chaque envoi sinon le serveur recoit pas correctement le signal
 		i++;
 	}
 }
 
-void	handle_ack(int sig)
+void	handle_ack(int sig) // fonction qui permet de dire au client que le serveur a bien recu le signal (BONUS)
 {
 	(void)sig;
 	ft_printf("Recu\n");
@@ -38,7 +38,7 @@ void	handle_ack(int sig)
 
 int	main(int argc, char **argv)
 {
-	int		pid;
+	int		pid; // pid du serveur (server.c)
 	int		i;
 
 	if (argc != 3)
@@ -47,13 +47,13 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	pid = ft_atoi(argv[1]);
-	signal(SIGUSR1, handle_ack);
+	signal(SIGUSR1, handle_ack); // on dit au client de recevoir le signal SIGUSR1 (BONUS)
 	i = 0;
-	while (argv[2][i])
+	while (argv[2][i]) // tant que le message n'est pas fini
 	{
-		send_signal(pid, argv[2][i]);
+		send_signal(pid, argv[2][i]); // on envoie le signal au serveur
 		i++;
 	}
-	send_signal(pid, '\0');
+	send_signal(pid, '\0'); // le null a la fin
 	return (0);
 }
--- server.c
+++ server.c
@@ -14,36 +14,36 @@
 #include "libft/libft.h"
 #include "ft_printf/ft_printf.h"
 
-void	handle_signal(int sig, siginfo_t *info, void *context)
+void	handle_signal(int sig, siginfo_t *info, void *context) // fonction qui permet de recevoir le signal (SIGUSR1 ou SIGUSR2)
 {
 	static int	bit = 0;
 	static int	character = 0;
 
 	(void)context;
-	if (sig == SIGUSR1)
-		character |= (1 << (7 - bit));
+	if (sig == SIGUSR1) // si le signal est SIGUSR1
+		character |= (1 << (7 - bit)); // decaler le bit de 7-i vers la droite et mettre le bit a 1
 	bit++;
-	if (bit == 8)
+	if (bit == 8) // si on a recu 8 bits (caractere complet)
 	{
-		write(1, &character, 1);
+		write(1, &character, 1); // on affiche le caractere
 		if (character == '\0')
 			write(1, "\n", 1);
 		bit = 0;
 		character = 0;
-		kill(info->si_pid, SIGUSR1);
+		kill(info->si_pid, SIGUSR1); // on envoie un signal au client pour dire qu'on a bien recu le caractere (BONUS)
 	}
 }
 
 int	main(void)
 {
-	struct sigaction	sa;
+	struct sigaction	sa; // structure qui permet de gerer les signaux
 
 	ft_printf("Server PID: %d\n", getpid());
-	sa.sa_sigaction = handle_signal;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	sa.sa_sigaction = handle_signal; // on dit a la structure de gerer le signal avec la fonction handle_signal
+	sa.sa_flags = SA_SIGINFO; // on dit a la structure de gerer le signal avec les infos du signal
+	sigemptyset(&sa.sa_mask); // on vide le masque de la structure
+	sigaction(SIGUSR1, &sa, NULL); // on dit a la structure de gerer le signal SIGUSR1 avec la fonction handle_signal
+	sigaction(SIGUSR2, &sa, NULL); // on dit a la structure de gerer le signal SIGUSR2 avec la fonction handle_signal
 	while (1)
 		pause();
 	return (0);
